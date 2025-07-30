bc6e0f163619e291c781849d869995050f35f178
Gisela-99 <giselacarballour@gmail.com>
2025-07-30 13:04:06 +0200

    cloudinary services

--- src/pages/AddGarment/AddGarment.jsx
+++ src/pages/AddGarment/AddGarment.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+//import { useState } from 'react';
+// import { uploadImageToCloudinary } from '../services/cloudinaryService';
 
-function AddGarment() {
+const AddGarment = () => {
+  // const [imageUrl, setImageUrl] = useState(null);
+  // const [uploading, setUploading] = useState(false);
+
+  // const handleImageUpload = async (event) => {
+  //   const file = event.target.files[0];
+  //   if (!file) return;
+
+  //   setUploading(true);
+
+  //   try {
+  //     const url = await uploadImageToCloudinary(file);
+  //     setImageUrl(url);
+  //     console.log('Imagen subida:', url);
+  //   } catch (err) {
+  //     console.error('Error al subir la imagen:', err);
+  //   } finally {
+  //     setUploading(false);
+  //   }
+  // };
+
   return (
     <div>
-      Añadir prenda
+      {/* <input type="file" accept="image/*" onChange={handleImageUpload} />
+      {uploading && <p>Subiendo imagen...</p>}
+      {imageUrl && (
+        <div>
+          <p>Imagen subida:</p>
+          <img src={imageUrl} alt="Subida" style={{ width: '300px' }} />
+        </div>
+      )} */}
+      <p>Añadir prenda</p>
     </div>
-  )
-}
+  );
+};
 
-export default AddGarment
+export default AddGarment;
